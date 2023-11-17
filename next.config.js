92046d0dfc49cf969f22262a10fcc8fda22a3e5c
jasoncollins111 <mailjasoncollins@gmail.com>
2023-11-17 14:21:55 -0700

    mvp

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'www.jambase.com',
+            port: '',
+            pathname: '/**',
+          },
+        ]
+    }
+}
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
 'use client'
-import { FormEvent, useEffect } from 'react'
+import { FormEvent, useCallback, useEffect } from 'react'
 import { useState } from 'react';
 import axios from 'axios';
-import {Button, Card, TextField, Typography} from '@mui/material';
+import {Box, Button, Card, Link, TextField, Typography} from '@mui/material';
+import {DateTime} from "luxon";
+import Image from 'next/image';
+
+interface Artist{
+  genre: string
+  name: string
+}
+
+interface Offer{
+  url: string
+}
 
 interface Event{
   name: string
   startDate: string
   image: string
+  performer: Array<Artist>
+  offers: Array<Offer>
+  location: {
+    name: string
+  }
 }
 
 export default function Home() {
@@ -17,27 +33,42 @@ export default function Home() {
 
   useEffect(()=> {
     getEvents();
-  },[])
+  })
 
   function mapEvents(eventList: any[]){
     const eventMap : any[] = eventList.map((event: Event, idx: number) => {
+      const {location, performer, startDate, offers} = event;
+      const date = DateTime.fromISO(startDate);
+      const month = date.month;
+      const day = date.day;
+      const genre = performer[0].genre[0];
+      const ticketLink = offers[0].url || '';
+      const venue = location.name;
+      const artist = performer[0].name;
+
       return(
         <Card key={idx} variant="outlined" className='mb-10'>
-          <img src={event?.image} width="500"/>
-          <Typography className='mx-3'>{event?.name} </Typography>
-          <Typography className='mx-3'>{event?.startDate} </Typography>
+          <Image src={event?.image} width="700" height="700" alt="Picture of artist"/>
+          <Box className='flex justify-between'>
+            <Typography className='mx-3'>{month}/{day} </Typography>
+            <Typography className='mx-3'>{artist} @ {venue}</Typography>
+            <Typography className='mx-3'>{genre}</Typography>
+          </Box>
+          <Box className='flex justify-end'>
+            <Link className='mx-3' href={ticketLink}>Tickets</Link>
+          </Box>
         </Card>
       )
     })
     setEvents(eventMap);
   } 
 
-  async function getEvents(event?: FormEvent<HTMLFormElement>) {
+  const getEvents = useCallback(async(event?: FormEvent<HTMLFormElement>) => {
     event?.preventDefault()
     const results = await axios.get('/api/events');
     const {data} = results;
     mapEvents(data)
-  }
+  }, [mapEvents])
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
