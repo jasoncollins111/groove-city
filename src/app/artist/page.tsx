a9667a0abf5a11928f395cdab5d02e0287100291
jasoncollins111 <mailjasoncollins@gmail.com>
2023-11-28 16:33:32 -0700

    artist page wip

--- src/app/artist/page.tsx
+++ src/app/artist/page.tsx
@@ -1,11 +1,52 @@
-import {Box} from '@mui/material';
+'use client'
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'next/navigation'
+import axios from 'axios';
+import Loading from '../loading';
+// import ArtistDetails from './artistDetails';
+interface EventData{
+    image: string
+    name: string
+    performer: [
+        Performer
+    ]
+    location: {
+        name: string
+    }
+    offers: [
+        {
+            url: string
+        }
+    ]
+  }
+interface Performer{
+    genre: [
+        string
+    ]
+    name: string
+    identifier: string
+    image: string
+}
+export default function Artist() {
+    const searchParams = useSearchParams()
+    const [artistData, setArtistData] = useState<EventData>({location: {name: ''}, offers: [{url: ''}], image: '', name: '', performer: [{genre:[''], name:'', identifier: '', image: ''}]});
 
-export default function Artists() {
+    useEffect(()=> {
+        getArtist();
+    },[])
+
+    async function getArtist(){
+        const id = searchParams?.get('performer')
+        console.log('getartist',id)
+        const results = await axios.get('/api/artist', { params: { id } });
+        console.log('results', results)
+        const {data} = results;
+        setArtistData(data);
+    }
+
     return (
         <main>
-            <Box>
-                Artist Page
-            </Box>
+            {/* {!artistData.image ? <Loading/> : <ArtistDetails artistData={artistData}/> } */}
         </main>
     )
 }
--- src/pages/api/artist.tsx
+++ src/pages/api/artist.tsx
@@ -3,9 +3,9 @@ import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const id = req?.query?.id;
+    const artistId = req?.query?.id;
     try {
-        const results = await axios.get(`https://www.jambase.com/jb-api/v1/events/id/${id}?apikey=${jambaseAPIKEY}`);
+        const results = await axios.get(`https://www.jambase.com/jb-api/v1/artists/id/${artistId}?apikey=${jambaseAPIKEY}`);
         const data = results?.data?.event
         return res.json(data);
         
