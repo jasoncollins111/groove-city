21df93bd1a6ed23433ad5c3c2ad2d0a5e3dfba25
jasoncollins111 <mailjasoncollins@gmail.com>
2024-02-15 09:55:29 -0700

    check build

--- src/app/artist/page.tsx
+++ src/app/artist/page.tsx
@@ -17,13 +17,12 @@ export default function Artist() {
 
     async function getArtist(){
         const id = searchParams?.get('performer')
-        // await login();
         const artistResults = await axios.get('/api/artist', { params: { id } });
-        // const artistEvents = await axios.get('/api/artistEvents', { params: { id } });
-        // const events = artistEvents.data;
+        const artistEvents = await axios.get('/api/artistEvents', { params: { id } });
+        const events = artistEvents.data;
         const {data} = artistResults;
         setArtist(data);
-        // setEvents(events);
+        setEvents(events);
     }
 
     async function login(){
